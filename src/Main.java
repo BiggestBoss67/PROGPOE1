0db6123880c723e3c74a4b44e6cecb5ac5341115
Student <st10536068@rcconnect.edu.za>
2026-04-10 12:45:52 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    Scanner input = new Scanner(System.in);
+    int count = 0;
+    int sum = 0;
+    int num;
+
+    while (count < 10){
+
+        System.out.println("Enter a number");
+        num = input.nextInt();
+        sum += num;
+        count++;
+    }
+System.out.println("Sum " + sum);
+}
